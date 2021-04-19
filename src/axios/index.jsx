626df99a6756eab06a41fbc2fecaf99aa2633c7a
Sourav3838 <54549602+Sourav3838@users.noreply.github.com>
2021-04-20 03:37:15 +0530

    changed base url of deployed database

--- src/axios/index.jsx
+++ src/axios/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 // https://tinder-mern-backend-practice.herokuapp.com/
-const instance = axios.create({ baseURL: "http://localhost:8000" });
+const instance = axios.create({ baseURL: "https://flybuy-backend.herokuapp.com/" });
 
 export default instance;
