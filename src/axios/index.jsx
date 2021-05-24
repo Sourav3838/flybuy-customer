06c73673f5b79ce650a9ed6adc8102ea1ec88187
Sourav3838 <54549602+Sourav3838@users.noreply.github.com>
2021-05-25 04:02:19 +0530

    Update index.jsx

--- src/axios/index.jsx
+++ src/axios/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // https://tinder-mern-backend-practice.herokuapp.com/
-// https://flybuy-backend.herokuapp.com/
-const instance = axios.create({ baseURL: 'http://localhost:8000/' });
+// http://localhost:8000/
+const instance = axios.create({ baseURL: 'https://flybuy-backend.herokuapp.com/' });
 
 export default instance;
